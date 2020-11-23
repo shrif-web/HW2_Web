7dc0e02c52005bbbe82921ad59b4aa0988a9eda5
esmaeil1377 <pahang1378@gmail.com>
2020-11-23 21:42:48 +0330

    every thing in homepage is ok but night mode

--- front/js/homepage.js
+++ front/js/homepage.js
@@ -1,8 +1,16 @@
+var x = 0;
 $(function() {
     // Sidebar toggle behavior
     $('#sidebarCollapse').on('click', function() {
       $('#sidebar').toggleClass('active');
-  
+      if (x % 2 == 0) {
+        $('#bar_icon').removeClass('fa-bars');
+        $('#bar_icon').addClass('fa-times');
+      } else {
+        $('#bar_icon').removeClass('fa-times');
+        $('#bar_icon').addClass('fa-bars');
+      }
+      x = x +1;
     });
   });
   
@@ -20,3 +28,15 @@ $(function() {
           }
       });
   });
+
+function onBarClick() {
+    $('#bar_icon').removeClass('fa-bars');
+    $('#bar_icon').addClass('fa-times');
+    if ($('.left_sidebar').css('visibility') == 'hidden') {
+        $('#bar_icon').removeClass('fa-bars');
+        $('#bar_icon').addClass('fa-times');
+    } else {
+        $('#bar_icon').removeClass('fa-times');
+        $('#bar_icon').addClass('fa-bars');
+    }
+}
