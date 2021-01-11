8b6198c11288ec5e945656d182ebc4a02423f0c6
sh3rLock3d <alipourghsaemi>
2021-01-11 12:56:15 +0330

    simple front for admin

--- backend/simple_front/front.js
+++ backend/simple_front/front.js
@@ -1,35 +1,73 @@
-function signup() {
-    
-    let email = document.getElementById('femailsignup').value;
-    let password = document.getElementById('fpasssignup').value;   
-    alert(`email: ${email}\npassword: ${password}`)
-    let URL = 'http://localhost:3000/api/signup';
-    
-    $.post(URL)
-    .done(function( data ) {
-        alert( "Data Loaded: " + data );
-    })
-    
-    
-};
-
-function getUser(){
-    let URL = 'http://localhost:3000/api/getuser';
-    $.post(URL)
-    .done(function( data ) {
-        alert( "Data Loaded: " + data );
-    })
-
+window.onload = function () {
+    document.getElementById("signinbutton").addEventListener("click", signin);
+    showPosts();
 }
 
-function signin(){
-    let email = document.getElementById('femailsignup').value;
-    let password = document.getElementById('fpasssignup').value;
-    let URL = 'http://localhost:3000/api/signin';
-    $.post(URL, {email, password})
-    .done(function( data ) {
-        alert( "Data Loaded: " + data );
-    })
 
 
+
+async function posthttp(data, url) {
+    var formBody = [];
+    for (var property in data) {
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(data[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    
+    var obj = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        body: formBody,
+    };
+    let response = await fetch(url, obj);
+    let responsemessage = await response.text();
+    return {body: responsemessage, status: response.status};
 }
+
+async function gethttp(url){
+    let response = await fetch(url);
+    let responsemessage = await response.text();
+    return {body: responsemessage, status: response.status};
+}
+
+function signin() {
+    let email = document.getElementById("femailsignin").value;
+    let password = document.getElementById("fpasssignin").value;
+    let data = {email, password};
+    document.getElementById('responseFromBack').innerHTML = "bye";
+ 
+    //alert(body);
+    let url = "http://localhost:3000/api/signin";
+    var response = posthttp(data, url);
+    response.then(function(res) {
+        alert(res.body + "\n" + res.status);
+    });
+}
+
+function showPosts(){
+    let postsDiv = document.getElementById("posts");
+    let url = "http://localhost:3000/api/post/";
+    let response = gethttp(url);
+    response.then(posts =>{
+        for (post of JSON.parse(posts.body).message){
+            var node = document.createElement("div");
+            for(element in post){
+                var para = document.createElement("p");
+                var paraNode = document.createTextNode(element +" : " + post[element]);
+                para.appendChild(paraNode);
+                node.appendChild(para);
+            }
+            node.style.border = "thick solid #0000FF";
+            node.style.margin = "10px";
+            node.style.padding = "10px";
+            postsDiv.appendChild(node);
+        }       
+    })
+}
+
+
+
+
