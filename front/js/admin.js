166899a21246ee3c394bca1e1a58294a73c00990
sh3rLock3d <alipourghsaemi>
2021-01-11 22:19:16 +0330

    front js

--- front/js/admin.js
+++ front/js/admin.js
@@ -1,5 +1,7 @@
 window.onload = function () {
     document.getElementById("createpost").addEventListener("click", createpost);
+    document.getElementById("updatepost").addEventListener("click", sendeditpost);
+    showPosts();
 }
 
 async function posthttp(data, url) {
@@ -43,11 +45,148 @@ function createpost() {
 }
 
 
+function showPosts(){
+    let postsDiv = document.getElementById("showPosts");
+    let url = "http://localhost:3000/api/post/";
+    let response = gethttp(url);
+    response.then(posts =>{
+        for (post of JSON.parse(posts.body).message){
+            var node = document.createElement("div");
+            node.classList.add("box2");
+            var title = document.createElement("h5");
+            title.classList.add("card-title");
+            title.style.textAlign = "center"
+            node.appendChild(title);
+            
+            title.appendChild(document.createTextNode(post['title']));
+            var auth = document.createElement("p");
+            auth.appendChild(document.createTextNode("نویسنده : "+post['created_by']));
+            auth.style.textAlign = "right"
+            auth.style.direction = "rtl"
+            node.appendChild(auth);
+  
+            var desc = document.createElement("p");
+            desc.appendChild(document.createTextNode(post['content']));
+            desc.style.textAlign = "right"
+            desc.style.height = "50px"
+            desc.style.overflow = "hidden"
+            node.appendChild(desc);
+            
+            const postId = post["id"];
+
+            var updateb = document.createElement("button");
+            updateb.classList.add('ui','primary', 'button')
+            updateb.innerHTML = "update"
+            updateb.onclick = function() {
+                updatePost(postId);
+            }
+            node.appendChild(updateb);
+
+
+            var deletebutton = document.createElement("button");
+            deletebutton.classList.add('ui','primary', 'button')
+            deletebutton.innerHTML = "delete"
+            //deletebutton.addEventListener("click", deletePost(postId));
+            deletebutton.onclick = function(){
+                deletePost(postId).then(msg =>{
+                    alert(msg.body + "\n" + msg.status);
+                    location.reload();
+                })
+            }
+            node.appendChild(deletebutton);
+
+            var read = document.createElement("button");
+            read.classList.add('ui','primary', 'button')
+            read.innerHTML = "read"
+            read.onclick = function() {
+                readPost()
+            }
+            node.appendChild(read);
+
+            node.style.margin = "5px";
+            node.style.padding = "10px";
+            //node.style.height = "150px"
+            //node.style.overflow = "hidden"
+            node.style.backgroundColor = "#A9A9A9"
+            postsDiv.appendChild(node);
+        }       
+    })
+}
 
 
 
+async function deletePost(id) {
+    const url = "http://localhost:3000/api/admin/post/crud/" + id ;
+    var obj = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+    };
+
+    let response = await fetch(url, obj);
+    let responsemessage = await response.text();
+    return { body: responsemessage, status: response.status };
+}
+
+function updatePost(id) {
+    let postsDiv = document.getElementById("allposts");
+    postsDiv.style.display = "none"
+    let updateDiv = document.getElementById("editpost");
+    updateDiv.idval = id
+    updateDiv.style.display = "block"
+}
+
+async function puthttp(data, url) {
+    var formBody = [];
+    for (var property in data) {
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(data[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    var obj = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        body: formBody,
+    };
+
+    let response = await fetch(url, obj);
+    let responsemessage = await response.text();
+    return { body: responsemessage, status: response.status };
+}
 
 
+function sendeditpost() {
+    let updateDiv = document.getElementById("editpost");
+    let url = 'http://localhost:3000/api/admin/post/crud/' + updateDiv.idval;
+    let title = document.getElementById("titleupdatepost").value;
+    let content = document.getElementById("contentupdatepost").value;
+    let data = { title, content };
+    puthttp(data, url).then(response => {
+        alert(response.body);
+    })
+    
+}
+
+function readPost(id) {
+    let postsDiv = document.getElementById("allposts");
+    postsDiv.style.display = "none"
+    let updateDiv = document.getElementById("readpost");
+    updateDiv.style.display = "block"
+    let url = 'http://localhost:3000/api/admin/post/crud/' + id;
+    gethttp(url).then(value => {
+        let mypost = value.body;   
+        alert(mypost)
+        document.getElementById("titleread").innerHTML = mypost.title;
+        document.getElementById("createdbyread").innerHTML = mypost['created_by'];
+        document.getElementById("contentread").innerHTML = JSON.stringify(mypost);
+    })
+
+}
 
 
 
--- front/js/login.js
+++ front/js/login.js
@@ -18,3 +18,145 @@ $('.btn-connection').click(function() {
   $('.btn-connection').removeClass('active');
 });
 });
+
+function validate_signup() {
+  var Email = document.getElementById("Email_signup").value;
+  var password = document.getElementById("password_signup").value;
+  var confirm_password = document.getElementById("confirm_password").value;
+  var checkbox = document.getElementById("c1")
+  var send = true;
+  if (Email.length == 0) {
+    document.getElementById("error3").innerHTML = "لطفا یک آدرس ایمیل وارد کنید";
+    error3.style.color = "red"
+    document.getElementById("Email_signup").style.border = "2px solid red"
+    send = false;
+  }
+  else {
+    document.getElementById("error3").innerHTML = "";
+    document.getElementById("Email_signup").style.border = "2px solid #0080ff"
+  }
+  if (password.length == 0) {
+    document.getElementById("error4").innerHTML = "لطفا رمز عبور را وارد کنید"
+    error4.style.color = "red"
+    document.getElementById("password_signup").style.border = "2px solid red"
+    var send = false;
+  }
+  else if (password.localeCompare(confirm_password)) {
+    document.getElementById("error4").innerHTML = "";
+    document.getElementById("password_signup").style.border = "2px solid #0080ff"
+    document.getElementById("error5").innerHTML = "تکرار رمز عبور با رمز عبور یکسان نیست"
+    error5.style.color = "red"
+    document.getElementById("confirm_password").style.border = "2px solid red"
+    var send = false;
+  }
+  else {
+    document.getElementById("error5").innerHTML = "";
+    document.getElementById("confirm_password").style.border = "2px solid #0080ff"
+  }
+  if (checkbox.checked == false) {
+    document.getElementById("error6").innerHTML = "لطفا قوانین و شرایط را بپذیرید";
+    error6.style.color = "red"
+    var send = false;
+  }
+  else {
+    document.getElementById("error6").innerHTML = "";
+  }
+
+  if(send) {
+    let email = document.getElementById("Email_signup").value;
+    let password = document.getElementById("password_signup").value;
+    let data = {email, password};
+ 
+    //alert(body);
+    let url = "http://localhost:3000/api/signup";
+    var response = posthttp(data, url);
+    response.then(function(res) {
+        alert(res.body + "\n" + res.status);
+        //
+        document.getElementById("Email_signup").value = "";
+        document.getElementById("password_signup").value = "";
+        document.getElementById("confirm_password").value = "";
+        goadmin();
+
+
+    });
+  }
+
+}
+
+
+function goadmin(){
+	window.open("admin.html","_self");
+}
+
+
+
+async function posthttp(data, url) {
+  var formBody = [];
+  for (var property in data) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(data[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
+  
+  var obj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      body: formBody,
+  };
+  let response = await fetch(url, obj);
+  let responsemessage = await response.text();
+  return {body: responsemessage, status: response.status};
+}
+
+
+
+function validate() {
+  var Email = document.getElementById("Email").value;
+  var password = document.getElementById("password").value;
+  var send = true;
+  if (Email.length == 0) {
+    document.getElementById("error1").innerHTML = "لطفا یک آدرس ایمیل وارد کنید";
+    error1.style.color = "red"
+    document.getElementById("Email").style.border = "2px solid red"
+    send = false;
+  }
+  else {
+    document.getElementById("error1").innerHTML = "";
+    document.getElementById("Email").style.border = "2px solid #0080ff"
+  }
+  if (password.length == 0) {
+    document.getElementById("error2").innerHTML = "لطفا رمز عبور را وارد کنید"
+    error2.style.color = "red"
+    document.getElementById("password").style.border = "2px solid red"
+    send = false;
+  }
+  else {
+    document.getElementById("error2").innerHTML = "";
+    document.getElementById("password").style.border = "2px solid #0080ff"
+  }
+
+  if (send) {
+    let email = document.getElementById("Email").value;
+    let password = document.getElementById("password").value;
+    let data = {email, password};
+ 
+    //alert(body);
+    let url = "http://localhost:3000/api/signin";
+    var response = posthttp(data, url);
+    response.then(function(res) {
+        alert(res.body + "\n" + res.status);
+        //
+        document.getElementById("Email_signup").value = "";
+        document.getElementById("password_signup").value = "";
+        document.getElementById("confirm_password").value = "";
+        goadmin();
+
+
+    });
+  }
+
+}
